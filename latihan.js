f970da292740304b6653bdf8b89f5b9fec46819c
M Haidar Hanif <git@mhaidarhanif.com>
2018-10-25 14:20:32 +0700

    Revise soal & latihan scripts

--- latihan.js
+++ latihan.js
@@ -1,29 +1,42 @@
+class Keranjang {
+  constructor(items) {
+    this.items = items || []
+  }
+
+  tambahBarang(item) {
+    this.items.push(item)
+  }
+
+  cekJumlahBarang() {
+    return this.items.length + " buah"
+  }
+}
 class Barang {
-    constructor(item, jumlah, harga){
-        this.item = item,
-        this.jumlah = jumlah,
-        this.harga = harga
-    }
+  constructor(item, jumlah, harga) {
+    this.item = item
+    this.jumlah = jumlah
+    this.harga = harga
+  }
 }
 
 class Hp extends Barang {
-    constructor(item, jumlah, harga, merk){
-        super(item, jumlah, harga)
-         this.merk = merk
-    }
+  constructor(item, jumlah, harga, merk) {
+    super(item, jumlah, harga)
+    this.merk = merk
+  }
 
-    updateBarang (item){
-        return this.item = item
-    }
+  updateBarang(item) {
+    return (this.item = item)
+  }
 }
 
-const keranjang= []
+const xiaomi = new Hp("hp", 2, 10000, "xiaomi")
+const sony = new Hp("hp", 2, 10000, "sony")
 
-const xiaomi = new Hp ('hp', 2, 10000, 'xiaomi')
-const sony = new Hp ('hp', 2, 10000, 'sony')
+const keranjang = new Keranjang()
 
-keranjang.push(xiaomi, sony)
-keranjang.splice(0,1)
-jumlahBarang = keranjang.length
+keranjang.tambahBarang(xiaomi)
+keranjang.tambahBarang(sony)
 
-console.log(jumlahBarang)
+console.log(keranjang)
+console.log(keranjang.cekJumlahBarang())
